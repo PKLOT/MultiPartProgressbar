15d75d2b5ea8e53975a688b46062712a7a6e0b57
CarterChen247 <carterchen247@gmail.com>
2020-11-06 12:41:39 +0800

    -

--- app/src/main/java/com/carterchen247/multiplepartprogressbar/demo/MainActivity.kt
+++ app/src/main/java/com/carterchen247/multiplepartprogressbar/demo/MainActivity.kt
@@ -6,6 +6,7 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.animation.DecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.carterchen247.multiplepartprogressbar.part.ProgressPart
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -42,32 +43,32 @@ class MainActivity : AppCompatActivity() {
     private fun getDefaultProgressbarParts(): List<ProgressPart> {
         return listOf(
             object : ProgressPart() {
-                override fun startColor() = Color.parseColor("#45b035")
-                override fun endColor() = Color.parseColor("#007f41")
+                override fun startColor() = ContextCompat.getColor(this@MainActivity, R.color.material_pink_200)
+                override fun endColor() = ContextCompat.getColor(this@MainActivity, R.color.material_pink_900)
                 override fun minValue() = 0
                 override fun maxValue() = 20000
             },
             object : ProgressPart() {
-                override fun startColor() = Color.parseColor("#f6ab00")
-                override fun endColor() = Color.parseColor("#ed6c00")
+                override fun startColor() = ContextCompat.getColor(this@MainActivity, R.color.material_yellow_200)
+                override fun endColor() = ContextCompat.getColor(this@MainActivity, R.color.material_yellow_900)
                 override fun minValue() = 20000
                 override fun maxValue() = 40000
             },
             object : ProgressPart() {
-                override fun startColor() = Color.parseColor("#fff78c")
-                override fun endColor() = Color.parseColor("#fdd000")
+                override fun startColor() = ContextCompat.getColor(this@MainActivity, R.color.material_green_200)
+                override fun endColor() = ContextCompat.getColor(this@MainActivity, R.color.material_green_900)
                 override fun minValue() = 40000
                 override fun maxValue() = 60000
             },
             object : ProgressPart() {
-                override fun startColor() = Color.parseColor("#37bef0")
-                override fun endColor() = Color.parseColor("#006fbc")
+                override fun startColor() = ContextCompat.getColor(this@MainActivity, R.color.material_blue_200)
+                override fun endColor() = ContextCompat.getColor(this@MainActivity, R.color.material_blue_900)
                 override fun minValue() = 60000
                 override fun maxValue() = 80000
             },
             object : ProgressPart() {
-                override fun startColor() = Color.parseColor("#e60012")
-                override fun endColor() = Color.parseColor("#a72126")
+                override fun startColor() = ContextCompat.getColor(this@MainActivity, R.color.material_purple_200)
+                override fun endColor() = ContextCompat.getColor(this@MainActivity, R.color.material_purple_900)
                 override fun minValue() = 80000
                 override fun maxValue() = 99999
             },
